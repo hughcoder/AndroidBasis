157061bee59977c5af947c70dc2f80ec24d0a6f9
chenyouwei <chenyouwei@qupeiyin.com>
2020-07-09 18:02:55 +0800

    feat:新增ffmpeg调试

---
+++ module-ffmpeg/src/main/cpp/ffmpeg-lib.cpp
@@ -0,0 +1,22 @@
+#include <jni.h>
+
+// 有坑，会报错，必须混合编译
+//#include <libavutil/avutil.h>
+
+extern "C" {
+#include <libavutil/avutil.h>
+}
+
+
+/**
+ * 拿到 ffmpeg 当前版本
+ * @return
+ */
+const char *getFFmpegVer() {
+    return av_version_info();
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_hugh_ffmpeg_CCMainActivity_getFFmpegVersion(JNIEnv *env, jclass type) {
+    return env->NewStringUTF(getFFmpegVer());
+}
--- module-ffmpeg/src/main/java/com/hugh/ffmpeg/CCMainActivity.java
+++ module-ffmpeg/src/main/java/com/hugh/ffmpeg/CCMainActivity.java
@@ -26,6 +26,7 @@ public class CCMainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("ffmpeg_lib");
     }
 
     @Override
@@ -61,6 +62,8 @@ public class CCMainActivity extends AppCompatActivity {
             NostaticRef();
         }
 
+        Log.e("aaa","getFFmpegVersion"+getFFmpegVersion());
+
 
 
 
@@ -69,6 +72,8 @@ public class CCMainActivity extends AppCompatActivity {
     public int getIntValue(){
         return 1;
     }
+
+    public native String getFFmpegVersion();
 
     public native String stringFromJNI();
     //返回静态字符串
